64dc4b1322db1f0dbc4e9344d5bc8836bbaa40f0
Dave Albert <dave.albert@gmail.com>
2024-01-08 07:32:40 +0000

    Added KV to function

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,12 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export async function onRequest(context) {
+    let counter = -1;
+    try {
+        const env = context.env;
+        counter = await env.KV_SANDBOX.get("counter", 0);
+        counter++;
+        await env.KV_SANDBOX.put("counter", counter);
+    } catch (error) {
+        console.log(error);
+    }
+    return new Response(`Hello, world! Visitor number ${counter}`);
   }
