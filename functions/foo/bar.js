66d2353b6106aa00a975a6287f30b90127059fb2
Dave Albert <dave.albert@gmail.com>
2024-01-09 19:46:55 +0000

    update foo/bar to deal wih zap

--- functions/foo/bar.js
+++ functions/foo/bar.js
@@ -1,3 +1,17 @@
-export function onRequest(context) {
-    return new Response("This is foo bar!")
+export async function onRequest(context) {
+    // recive a json post request
+    const request = context.request;
+    const body = request.body;
+    const json = JSON.parse(body);
+    const env = context.env;
+
+    const zipzap = await env.KV_SANDBOX.get("zipzap", "text");
+
+    if (zipzap !== null && zipzap === json.zipzap && json.zipzap !== "") {
+        // save json to KV store with key last video
+        await env.KV_SANDBOX.put("last_video", body);
+        return new Response(JSON.stringify({status: "ok"}), { status: 200, headers: { 'Content-Type': 'application/json' } });
+    }
+
+    return new Response(JSON.stringify({status: "unauthorized"}), { status: 401, headers: { 'Content-Type': 'application/json' } });
   }
